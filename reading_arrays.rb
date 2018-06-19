631cb1b410a3e5803c7b0fb075bd6dacfc3789e4
shaygyawali <shaygyawali@gmail.com>
2018-06-19 19:11:10 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -30,11 +30,11 @@ end
 first_student_by_method
 
 def last_student_by_method
-  puts STUDENTS_NAMES.last
+  puts STUDENT_NAMES.last
 end
 last_student_by_method
 
 def first_second_and_third_students
-  puts STUDENTS_NAMES[0,1,2]# Write a solution that returns the first, second and third students
+  puts STUDENT_NAMES[0,1,2]# Write a solution that returns the first, second and third students
 end
 first_second_and_third_students
